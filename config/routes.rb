e8f8a3178648d03384079dee744d8546c38b7d35
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-09-03 11:50:25 +0000

    [start]comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy] do
      resource :favorite, only: [:create, :destroy]
+     resources :book_comments, only: [:create]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

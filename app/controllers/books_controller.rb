6db2c97b08eefd3ef4409e2a8f4cbccea21d3541
Ryoichiro Kitahara <175901292+ryoichi1470@users.noreply.github.com>
2024-09-05 02:06:04 +0000

    [finish]comment2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @book_new = Book.new
-    @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
@@ -25,11 +24,9 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -38,7 +35,6 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
@@ -48,7 +44,7 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
   def ensure_correct_user
     @book = Book.find(params[:id])
     unless @book.user == current_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "homes#top"
-  get 'home/about', to: 'homes#about', as: 'about'
+  root :to => "homes#top"
+  get "home/about"=>"homes#about"
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:new, :index, :show, :create, :edit, :update, :destroy] do
-     resource :favorite, only: [:create, :destroy]
-     resources :book_comments, only: [:create]
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
